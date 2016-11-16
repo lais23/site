6b8ad6f7f6f884e66fb40a224ba990c7aad8002b
lais_naihaus <lais_naihaus@SNRSL1ELBIET048>
2016-11-16 08:05:55 -0200

    1 - criação do estilo css 2 - banner 3- criado o menu

--- index.php
+++ index.php
@@ -7,9 +7,26 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title> Ponto.com </title>
+        <link rel="stylesheet" href="css/estilo.css"/>
     </head>
     <body>
+        <div id="container">
+            <div id="header">
+                <img src=""/>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a href="?pg=home"> Home </a></li>
+                    <li><a href="?pg=produtos"> Produtos </a></li>
+                    <li><a href="?pg=promocoes"> Promoções </a></li>
+                    <li><a href="?pg=servicos"> Serviços </a></li>
+                    <li><a href="?pg=faq"> FAQ </a></li>
+                    <li><a href="?pg=contato"> Contato </a></li>
+                </ul>
+            </div>
+
+        </div>
         <?php
         // put your code here
         ?>
